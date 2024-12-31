914102c7b2463e506bff963a68875772f5485ef0
BhaskarBVV <bhaskar.nv3@gmail.com>
2024-12-31 19:20:32 +0530

    feat: Add environment variable check during application start

--- src/server/app.py
+++ src/server/app.py
@@ -16,6 +16,15 @@ api = Api(
 for route in ROUTES:
     api.add_resource(route[0], route[1])
 
+if os.getenv("MONGO_URI") is None:
+    raise KeyError("Mongodb connection string not found in environment")
+
+if os.getenv("ENCRYPTION_SECRET_KEY") is None:
+    raise KeyError("SHA 256 encryption-decryption key not found in environment")
+
+if os.getenv("HOST") is None:
+    raise KeyError("Host IP is needed")
+
 if __name__ == "__main__":
     host = os.getenv("HOST") or None
     app.run(host=host, debug=True, port=8080)
